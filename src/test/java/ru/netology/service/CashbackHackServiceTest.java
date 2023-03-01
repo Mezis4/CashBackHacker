5d5421a5336352edfcf36a31caf4f9c76f65fe63
mezitest <mezis4u@inbox.ru>
2023-03-01 22:19:46 +0600

    add tests with junit4

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,49 @@
+package ru.netology.service;
+
+import static org.junit.Assert.*;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @org.junit.Test
+    public void showRemainValue0() {
+        org.junit.Assert.assertEquals(1000, service.remain(0));
+    }
+
+    @org.junit.Test
+    public void showRemainValue1() {
+        org.junit.Assert.assertEquals(999, service.remain(1));
+    }
+
+    @org.junit.Test
+    public void showRemainValueBetweenBoundaries() {
+        org.junit.Assert.assertEquals(275, service.remain(725));
+    }
+
+    @org.junit.Test
+    public void showRemainValue999() {
+        org.junit.Assert.assertEquals(1, service.remain(999));
+    }
+
+    @org.junit.Test
+    public void showRemainValue1000() {
+        org.junit.Assert.assertEquals(0, service.remain(1000));
+    }
+
+    @org.junit.Test
+    public void showRemainValue1001() {
+        org.junit.Assert.assertEquals(999, service.remain(1001));
+    }
+
+    @org.junit.Test
+    public void negativeValueCheck() {
+        org.junit.Assert.assertEquals(1000, service.remain(-1));
+    }
+
+    @org.junit.Test
+    public void throwExceptionIfNegativeValue() {
+        org.junit.Assert.assertThrows(RuntimeException.class, () -> {
+            service.remain(-1);
+        });
+    }
+}
