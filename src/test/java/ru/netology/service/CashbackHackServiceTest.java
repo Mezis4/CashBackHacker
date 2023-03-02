9b41520692b9e665b49270f69710e7e5bbdac51d
mezitest <mezis4u@inbox.ru>
2023-03-02 12:59:13 +0600

    add tests with Jupiter

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -46,4 +46,49 @@ public class CashbackHackServiceTest {
             service.remain(-1);
         });
     }
+
+
+    @org.junit.jupiter.api.Test
+    public void jupiterShowRemainValue0() {
+        org.junit.jupiter.api.Assertions.assertEquals(1000, service.remain(0));
+
+    }
+
+    @org.junit.jupiter.api.Test
+    public void jupiterShowRemainValue1() {
+        org.junit.jupiter.api.Assertions.assertEquals(999, service.remain(1));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void jupiterShowRemainValueBetweenBoundaries() {
+        org.junit.jupiter.api.Assertions.assertEquals(275, service.remain(725));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void jupiterShowRemainValue999() {
+        org.junit.jupiter.api.Assertions.assertEquals(1, service.remain(999));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void jupiterShowRemainValue1000() {
+        org.junit.jupiter.api.Assertions.assertEquals(0, service.remain(1000));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void jupiterShowRemainValue1001() {
+        org.junit.jupiter.api.Assertions.assertEquals(999, service.remain(1001));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void jupiterNegativeValueCheck() {
+        org.junit.jupiter.api.Assertions.assertEquals(1000, service.remain(-1));
+
+    }
+
+    @org.junit.jupiter.api.Test
+    public void jupiterThrowExceptionIfNegativeValue() {
+        org.junit.jupiter.api.Assertions.assertThrows(RuntimeException.class, () -> {
+            service.remain(-1);
+        });
+    }
 }
